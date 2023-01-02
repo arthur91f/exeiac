7d8b730db9b797c58fbdf5303d3e73005c02eca9
arthur91f <arthur91f@gmail.com>
2023-01-02 18:51:49 +0100

    chore(example/module): improve them

---
+++ example/repos/modules/manual-module.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+ACTION="$1"
+ALL_ARGS="$@"
+CURRENT_PATH="$(pwd)"
+FILES_LIST="$EXEIAC_files_list"
+
+function show_implemented_actions {
+    grep "^function " $0 |
+        sed 's|^function \([^ ]*\) .*$|\1|g' |
+        grep -v "internal"
+    exit 0
+}
+
+function init {
+    echo "execute init: install tools and deps for $PWD"
+
+    status_code=0
+    python3 --version 2>/dev/null
+    if [ "$?" != 0 ]; then
+        echo "manual-module:init: python3 not installed"
+        return 1
+    fi
+    if which jq >/dev/null ; then
+        echo "manual-module:init: jq installed"
+    else
+        echo "manual-module:init: jq not installed"
+        echo "  https://stedolan.github.io/jq/download/"
+        status_code=1
+    fi
+}
+
+function internal_ask_confirmation {
+    echo -e "\033[1m$1\033[0m\033[3m(only yes accepted): \033[0m"
+    read answer
+    if [ "$answer" == "yes" ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+function internal_get_file_basename {
+    sed -e 's/.json$//g' -e 's/\.yml$//g' -e 's/\.yaml$//g' <<<"$1"
+}
+
+function plan {
+    if ! validate_code; then
+        return 3
+    fi
+    if grep -q ".*--non-interactive" <<<"$ALL_ARGS" ; then
+        echo "manual-module:plan: as the lay is a manual and the non-interactive mode is enabled, assume everything is ok"
+    else
+        echo "manual-module:plan: as the lay is a manual and the non-interactive mode is enabled, assume everything is ok"
+    fi
+}
+
+function lay {
+    if ! validate_code; then
+        return 3
+    fi
+    if grep -q ".*--non-interactive" <<<"$ALL_ARGS" ; then
+        echo "manual-module:lay: as the lay is a manual and the non-interactive mode is enabled, assume everything is ok"
+    else
+        output
+        if internal_ask_confirmation "Do you want to change anything ?" ; then
+            for file in $FILES_LIST ; do
+                file_basename="$(internal_get_file_basename "$file")"
+                if internal_ask_confirmation "Do you want to modify $file_basename ?" ; then
+                    nano $file
+                fi
+            done
+        fi
+    fi 
+}
+
+function remove {
+    for file in $FILES_LIST ; do
+        file_basename="$(internal_get_file_basename <<<"$file")"
+        echo "" > $file
+        echo "$file_basename"
+    done
+}
+
+function output {
+    (echo "{" ; for file in $FILES_LIST ; do
+        if grep -q "\.json$" <<<"$file"; then
+            file_basename="$(internal_get_file_basename "$file")"
+            echo -n "\"$file_basename\": {$(cat $file)},"
+        elif grep -Eq "\.(yml|yaml)" <<<"$file"; then
+            file_basename="$(internal_get_file_basename "$file")"
+            json="$(python3 -c 'import sys, yaml, json; y=yaml.safe_load(sys.stdin.read()); print(json.dumps(y))' < $file)"
+            echo -n "\"$file_basename\": $json,"
+        else
+            echo "manual-module:output: format not recognized or supported for $file" >&2
+            return 1
+        fi
+    done | sed 's/,$//g'; echo "}") | jq .
+}
+
+function validate_code {
+    list="$(for file in $FILES_LIST ; do
+        internal_get_file_basename "$file"
+    done | sort | uniq -c | sed '/^ *1/d')"
+    
+    if [ -n "$list" ]; then
+        echo "Some files are in double : "
+        list="$(echo "$list" | sed 's/^ *[0-9]* //g')"
+        for item in $list ; do
+            grep "$item" <<<"$FILES_LIST"
+        done
+        return 1
+    fi
+}
+
+if grep -q "^$ACTION$" <(show_implemented_actions) ; then
+    $ACTION
+    exitcode="$?"
+else
+    echo "action not implemented: $ACTION"
+    exitcode=3
+fi
+
+exit "$exitcode"
--- example/repos/modules/terraform.sh
+++ example/repos/modules/terraform.sh
@@ -1,5 +1,115 @@
 #!/bin/bash
+ACTION="$1"
+ALL_ARGS="$@"
+CURRENT_PATH="$(pwd)"
 
-echo "INFO: we use module test for the moment" >&2
-"$(dirname "$0")/module_test.sh" "$@"
+function show_implemented_actions {
+    grep "^function " $0 |
+        sed 's|^function \([^ ]*\) .*$|\1|g' |
+        grep -v "internal"
+    exit 0
+}
 
+function internal_quit {
+    text="$1"
+    exit_code="$2"
+
+    if [ -z "$text" ]; then
+        true
+    elif [ "$(wc -l <<<"$text")" == 1 ]; then
+        echo "$text" >&2
+    else
+        indent="terraform:"
+        while read line ; do
+            echo "$indent$line" >&2
+            indent="    "
+        done <<<"$text"
+    fi
+
+    exit $exit_code
+
+}
+
+function init {
+    echo "execute init: install tools and deps for $PWD"
+
+    status_code=0
+    terraform version 2>/dev/null
+    if [ "$?" != 0 ]; then
+        echo "terraform:init: terraform not installed"
+        echo "  https://developer.hashicorp.com/terraform/downloads"
+        return 1
+    fi
+    if which jq >/dev/null ; then
+        echo "terraform:init: jq installed"
+    else
+        echo "terraform:init: jq not installed"
+        echo "  https://stedolan.github.io/jq/download/"
+        status_code=1
+    fi
+
+    terraform init
+}
+
+#function internal_ask_confirmation {
+#     echo -e "\033[1m$1\033[0m\033[3m(only yes accepted): \033[0m"
+#     read answer
+#     if [ "$answer" == "yes" ]; then
+#         return 0
+#     else
+#         return 1
+#     fi
+# }
+
+function plan {
+    terraform plan -detailed-exitcode
+    case "$?" in
+    1)
+        return 2 ;;
+    2)
+        return 1 ;;
+    *)
+        return $? ;;
+    esac
+}
+
+function lay {
+    if grep -q ".*--non-interactive" <<<"$ALL_ARGS" ; then
+        terraform apply -auto-approve
+    else
+        terraform apply
+    fi
+}
+
+function remove {
+    if grep -q ".*--non-interactive" <<<"$ALL_ARGS" ; then
+        terraform destroy -auto-approve
+    else
+        terraform destroy
+    fi
+}
+
+function output {
+    terraform output -json | jq 'map_values(.value)'
+}
+
+function clean {
+    rm -r ./.terraform || 
+        internal_quit "clean:error when 'rm -r .terraform'" 2
+    rm ./.terraform.lock.hcl ||
+        internal_quit "clean:error when 'rm -r .terraform.lock.hcl'" 2
+    if [ "$(output)" == "{}" ]; then
+        rm ./terraform.tfstate || 
+            internal_quit "clean:error when 'rm terraform.tfstate'" 2
+    fi
+}
+
+if grep -q "^$ACTION$" <(show_implemented_actions) ; then
+    $ACTION
+    exitcode="$?"
+else
+    echo "action not implemented: $ACTION"
+    exitcode=3
+fi
+
+exit "$exitcode"
